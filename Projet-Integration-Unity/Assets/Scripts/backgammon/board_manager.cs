80f4eaaf20e13459370b19beb9ffc626b8a4a36e
Azur18 <azurcomeau@gmail.com>
2026-02-05 10:40:55 -0500

    ...well well well

--- Projet-Integration-Unity/Assets/Scripts/backgammon/board_manager.cs
+++ Projet-Integration-Unity/Assets/Scripts/backgammon/board_manager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class physicdice : MonoBehaviour
+public class board_manager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
