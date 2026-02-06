5c5df4ffffed0c75a06810f860cda0710beaf712
kojacks17 <alexbernier1017@gmail.com>
2026-02-06 00:33:07 -0500

    pathfinfing sans diagonale ni obstacle

---
+++ Projet-Integration-Unity/Assets/Scripts/Grid_Astar.cs
@@ -0,0 +1,139 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class GridManager
+{
+    int largeur = 37;
+    int hauteur = 29;
+
+    Node[,] grid;
+
+    public GridManager()
+    {
+        grid = new Node[largeur, hauteur];
+
+        for(int x = 0; x < largeur; x++) {
+            for(int y = 0; y < hauteur; y++)
+            {
+                grid[x,y] = new Node(x,y,true);
+            }
+        }
+    }
+
+    public List<Node> TrouverVoisins(Node node)
+    {
+        List<Node> voisins = new List<Node>();
+
+        int x = node.x;
+        int y = node.y;
+
+        if (x + 1 < largeur)
+            voisins.Add(grid[x + 1, y]);
+
+        if (x - 1 >= 0)
+            voisins.Add(grid[x - 1, y]);
+
+        if (y + 1 < hauteur)
+            voisins.Add(grid[x, y + 1]);
+
+        if (y - 1 >= 0)
+            voisins.Add(grid[x, y - 1]);
+
+        return voisins;
+    }
+    public void ResetNodes()
+    {
+        for (int x = 0; x < largeur; x++)
+        {
+            for (int y = 0; y < hauteur; y++)
+            {
+                grid[x, y].gCost = int.MaxValue;
+                grid[x, y].parent = null;
+            }
+        }
+    }
+
+    public Node GetNode(int x, int y)
+    {
+        return grid[x, y];
+    }
+
+    public List<Node> FindPath(Node startNode, Node targetNode)
+    {
+        ResetNodes();
+
+        List<Node> openSet = new List<Node>();
+        HashSet<Node> closedSet = new HashSet<Node>();
+
+        startNode.gCost = 0;
+        startNode.hCost = GetDistance(startNode, targetNode);
+
+        openSet.Add(startNode);
+
+        while (openSet.Count > 0)
+        {
+            Node currentNode = openSet[0];
+
+            for (int i = 1; i < openSet.Count; i++)
+            {
+                if (openSet[i].fCost < currentNode.fCost ||
+                (openSet[i].fCost == currentNode.fCost &&
+                    openSet[i].hCost < currentNode.hCost))
+                {
+                    currentNode = openSet[i];
+                }
+            }
+
+            openSet.Remove(currentNode);
+            closedSet.Add(currentNode);
+
+            if (currentNode == targetNode)
+            {
+                return RetracePath(startNode, targetNode);
+            }
+
+            foreach (Node neighbour in TrouverVoisins(currentNode))
+            {
+                if (!neighbour.walkable || closedSet.Contains(neighbour))
+                    continue;
+
+                int newCost = currentNode.gCost + 1;
+
+                if (newCost < neighbour.gCost || !openSet.Contains(neighbour))
+                {
+                    neighbour.gCost = newCost;
+                    neighbour.hCost = GetDistance(neighbour, targetNode);
+                    neighbour.parent = currentNode;
+
+                    if (!openSet.Contains(neighbour))
+                        openSet.Add(neighbour);
+                }
+            }
+        }
+
+        return null;
+    }
+
+
+    int GetDistance(Node a, Node b)
+        {
+            return Mathf.Abs(a.x - b.x) + Mathf.Abs(a.y - b.y);
+        }
+
+    List<Node> RetracePath(Node startNode, Node endNode)
+    {
+        List<Node> path = new List<Node>();
+        Node current = endNode;
+
+        while (current != startNode)
+        {
+            path.Add(current);
+            current = current.parent;
+        }
+
+        path.Reverse();
+        return path;
+    }
+
+}
--- Projet-Integration-Unity/Assets/Scripts/Spawn.cs
+++ Projet-Integration-Unity/Assets/Scripts/Spawn.cs
@@ -6,7 +6,7 @@ public class Spawn : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject prefab;
-    Vector3 spawn_1 = new Vector3(-0.66f,0.5f,0);//5.67f
+    Vector3 spawn_1 = new Vector3(-7f,1f,0);//5.67f
       Vector3 spawn_2 = new Vector3(25.6f,0.5f,0);
       Quaternion spawnRotation = Quaternion.identity;
       float timer = 0f;
@@ -21,13 +21,13 @@ public class Spawn : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        timer += Time.deltaTime;
+        /*timer += Time.deltaTime;
         if (timer >= spawn_delay )
         {
             Vector3 choix = Random.value < 0.5f ? spawn_1 : spawn_2;
             Instantiate(prefab,choix,spawnRotation);
             timer = 0f;
-        }
+        }*/
     }
 
 
