aac77d7e1515a57b76b688a0cfeac6e5c5c26054
Azur18 <azurcomeau@gmail.com>
2026-04-13 11:34:39 -0400

    ig

--- Projet-Integration-Unity/Assets/Scripts/backgammon/customArrays.cs
+++ Projet-Integration-Unity/Assets/Scripts/backgammon/customArrays.cs
@@ -3,7 +3,6 @@ abstract public class customArray
 {
 	public int index;
 	public int moveDepth;
-	public static short[] moveScores;
 	public int size()
 	{
 		return index;
@@ -15,7 +14,6 @@ class simpleMoveArray : customArray{
 	public simpleMoveArray()
 	{
 		moves = new ushort[2*15*15];
-		moveScores = new short[2*15*15]; // Can be made to be only one array use for all move
 		index = 0;
 		moveDepth = 0;
 	}
@@ -27,8 +25,7 @@ class doubleMoveArray : customArray{
 	public uint[] moves;
 	public doubleMoveArray()
 	{
-		moves = new uint[15 * 15 * 15 * 15]; // maybe 15*14*13*12*11 instead
-		moveScores = new short[15 * 15 * 15 * 15];
+		moves = new uint[15 * 15 * 15 * 15];
 		index = 0;
 		moveDepth = 0;
 	}
